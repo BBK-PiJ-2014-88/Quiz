832b8365eec6087eb9f55ee7ce3035540917c614
Sergio <rurounisergii@hotmail.com>
2015-02-13 18:01:40 +0000

    Moved everything in QuizServer to QuizLauncher

--- Quiz/src/server/QuizServer.java
+++ Quiz/src/server/QuizServer.java
@@ -1,29 +1,17 @@
 package server;
 
-import java.util.HashMap;
 
 import quiz.*;
 import gui.*;
 
-import java.io.*;
 
-public class QuizServer {
-	HashMap<Integer, Quiz> quizList = new HashMap<Integer, Quiz>();
-	
-	public QuizServer(){
-		File storageFile = new File("QuizStorage.txt");
-		if (storageFile.exists()){
-			System.out.println("File exists");
-			try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(storageFile))){
-				quizList = (HashMap<Integer, Quiz>) in.readObject();
-				System.out.println("Read the QuizList from File");
-			} catch(IOException e){
-				e.printStackTrace();
-			} catch (ClassNotFoundException ex){
-				ex.printStackTrace();
-			}
-		}
-	}
+/**
+ * 
+ * @author Sergio
+ * This is the central Quiz Server
+ *
+ */
+public class QuizServer {	
 	
 	public static void main(String[] args){
 		new QuizServer().launch();
@@ -31,31 +19,6 @@ public class QuizServer {
 	public void launch(){
 		
 	}
-	public void addQuiz(Quiz quizToAdd, int id){
-		
-	}
-	public Quiz getQuiz(int id){
-		return null;
-	}
-	public void deleteQuiz(int id){
-		
-	}
 	
-	
-	
-	public void flush(){
-		File storageFile = new File("QuizStorage.txt");
-		if (storageFile.exists()){
-			storageFile.delete();
-		}
-		try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(storageFile))){
-			out.writeObject(quizList);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-	}
 
 }
