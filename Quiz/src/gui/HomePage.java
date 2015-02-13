6d126b2dc96c78ad14e3ae2cf84b4fd14282a6fa
Sergio <rurounisergii@hotmail.com>
2015-02-13 12:26:21 +0000

    Added 2 JBUttons

--- Quiz/src/gui/HomePage.java
+++ Quiz/src/gui/HomePage.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class HomePage {
@@ -10,6 +11,10 @@ public class HomePage {
 	public void launch(){
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton makeQuizButton = new JButton("Click here to make a Quiz");
+		JButton playQuizButton = new JButton("Click here to play a Quiz");
+		frame.getContentPane().add(BorderLayout.NORTH, makeQuizButton);
+		frame.getContentPane().add(BorderLayout.SOUTH, playQuizButton);
 		frame.setSize(500,500);
 		frame.setVisible(true);
 	}
