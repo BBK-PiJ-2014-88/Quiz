0e0260c41d5f669bc397af8073422b42f5c5b69b
Sergio <rurounisergii@hotmail.com>
2015-02-13 19:28:21 +0000

    Added SetUpQuizGui

--- Quiz/src/client/SetUpClient.java
+++ Quiz/src/client/SetUpClient.java
@@ -1,11 +1,22 @@
 package client;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.*;
+
+
 public class SetUpClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//will connect to Server object on registry. 
 		//after making a quiz, will call ServerObject.saveQuiz()
+	}
+	public void launch(){
+		try {
+			Registry reg = LocateRegistry.getRegistry("localhost", 1099);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ Quiz/src/gui/SetUpQuizGui.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class SetUpQuizGui {
+	//first jpanel = name the quiz, move into setQuestion1, in a while loop until presses SaveQuiz
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
