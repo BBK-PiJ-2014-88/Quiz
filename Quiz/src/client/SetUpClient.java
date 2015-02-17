188b4ba118b4916c0e44f8a86eb048ed5b3ccab0
Sergio <rurounisergii@hotmail.com>
2015-02-17 13:16:52 +0000

    Added comments to the SetUpClient for explanation

--- Quiz/src/client/SetUpClient.java
+++ Quiz/src/client/SetUpClient.java
@@ -4,9 +4,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.*;
 import java.util.Arrays;
-
 import javax.swing.JOptionPane;
-
 import gui.*;
 import server.*;
 import quiz.*;
@@ -15,12 +13,19 @@ public class SetUpClient {
 	private Quiz newQuiz;
 	private QuizRemoteInterface remoteServerObject; //the remote server object in the registry
 	
+	/**
+	 * Makes a connection with the remote QuizServer object on the registry
+	 * and then launches the GUI for a user to enter a Quiz name
+	 */
 	public void launch(){
-		//First the SetUpClient makes a connection with the remote QuizServer object on the registry
 		connectToServer();
 		NamingQuizGui questionNameGui = new NamingQuizGui(this);
 		questionNameGui.launch();
 	}
+	/**
+	 * Creates a Quiz Object with a user supplied name
+	 * Involves invoking createQuizId() method on the QuizServer
+	 */
 	public void createQuiz(String quizName){
 		try {
 			newQuiz = new Quiz(quizName, remoteServerObject.createQuizId());
@@ -30,6 +35,9 @@ public class SetUpClient {
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * Adds a Quiz to the Quiz Server so a PlayerClient can play the quiz
+	 */
 	public void addQuizToServer(){
 		try {
 			remoteServerObject.addQuiz(newQuiz, newQuiz.getQuizId());
@@ -37,17 +45,26 @@ public class SetUpClient {
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * launches the GUI for a user to enter a  Question
+	 */
 	public void getQuizQuestions(){
 		EnterQuestionFrame enterQuestionGui = new EnterQuestionFrame(this);
 		enterQuestionGui.launch();
 	}
-	
+	/**
+	 * @param the question
+	 * @param the answers 
+	 * @param the correctAnswer int
+	 * adds a Question Object to the Quiz
+	 */
 	public void addQuestionToQuiz(String question, String[] answers, int correctAnswer){
 		this.newQuiz.addQuestion(new Question(question, answers, correctAnswer));
 		System.out.println("Successfully added question with question" + question + "answer + " + Arrays.toString(answers) + "correct answer" + correctAnswer);
 	}
-	
+	/**
+	 * Locates the Quiz Server object on the registry so methods can be invoked on it
+	 */
 	public boolean connectToServer(){
 		try {
 			Registry reg = LocateRegistry.getRegistry("localhost", 1099);
