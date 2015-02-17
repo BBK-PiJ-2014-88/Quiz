d77b08d4917b7c8d044f1a0d54c4924c50a264b6
Sergio <rurounisergii@hotmail.com>
2015-02-17 21:36:28 +0000

    Deleted old AnswerQuestionFrame and made new AnswerQuestionFrame class which is an application window instead of an extension of JFrame. Changed the layout so there are 4 buttons for 4 possible answers instead of 4 labels and 4 JRadioButtons

--- Quiz/src/gui/AnswerQuestionFrame.java
+++ Quiz/src/gui/AnswerQuestionFrame.java
@@ -1,25 +1,16 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JRadioButton;
-import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JButton;
 
-public class AnswerQuestionFrame extends JFrame {
+public class AnswerQuestionFrame {
 
-	private JPanel contentPane;
-	private JTextField possibleAnswer1;
-	private JTextField possibleAnswer3;
-	private JTextField possibleAnswer2;
-	private JTextField possibleAnswer4;
+	private JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -28,8 +19,8 @@ public class AnswerQuestionFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AnswerQuestionFrame frame = new AnswerQuestionFrame();
-					frame.setVisible(true);
+					AnswerQuestionFrame window = new AnswerQuestionFrame();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,67 +29,62 @@ public class AnswerQuestionFrame extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public AnswerQuestionFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 628, 448);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 607, 464);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel questionLabel = new JLabel("Question displayed here");
-		questionLabel.setForeground(Color.RED);
-		questionLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		questionLabel.setBounds(45, 28, 524, 76);
-		contentPane.add(questionLabel);
+		JLabel questionLabel = new JLabel("Question goes here");
+		questionLabel.setBackground(Color.YELLOW);
+		questionLabel.setForeground(new Color(0, 0, 0));
+		questionLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		questionLabel.setBounds(40, 71, 517, 65);
+		frame.getContentPane().add(questionLabel);
 		
-		possibleAnswer1 = new JTextField();
-		possibleAnswer1.setColumns(10);
-		possibleAnswer1.setBounds(45, 180, 165, 20);
-		contentPane.add(possibleAnswer1);
+		JLabel questionNumberLabel = new JLabel("QuestionNumber");
+		questionNumberLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		questionNumberLabel.setBounds(40, 34, 127, 14);
+		frame.getContentPane().add(questionNumberLabel);
 		
-		possibleAnswer3 = new JTextField();
-		possibleAnswer3.setColumns(10);
-		possibleAnswer3.setBounds(45, 243, 165, 20);
-		contentPane.add(possibleAnswer3);
+		JButton answer1Button = new JButton("New button");
+		answer1Button.setBackground(Color.YELLOW);
+		answer1Button.setForeground(Color.RED);
+		answer1Button.setBounds(82, 200, 159, 78);
+		frame.getContentPane().add(answer1Button);
 		
-		possibleAnswer2 = new JTextField();
-		possibleAnswer2.setColumns(10);
-		possibleAnswer2.setBounds(314, 180, 165, 20);
-		contentPane.add(possibleAnswer2);
+		JButton answer2Button = new JButton("New button");
+		answer2Button.setForeground(Color.RED);
+		answer2Button.setBackground(Color.YELLOW);
+		answer2Button.setBounds(357, 200, 159, 78);
+		frame.getContentPane().add(answer2Button);
 		
-		possibleAnswer4 = new JTextField();
-		possibleAnswer4.setColumns(10);
-		possibleAnswer4.setBounds(314, 243, 165, 20);
-		contentPane.add(possibleAnswer4);
+		JButton answer3Button = new JButton("New button");
+		answer3Button.setForeground(Color.RED);
+		answer3Button.setBackground(Color.YELLOW);
+		answer3Button.setBounds(82, 311, 159, 78);
+		frame.getContentPane().add(answer3Button);
 		
-		JRadioButton answer1RadioButton = new JRadioButton("New radio button");
-		answer1RadioButton.setBounds(227, 179, 18, 23);
-		contentPane.add(answer1RadioButton);
+		JButton answer4Button = new JButton("New button");
+		answer4Button.setForeground(Color.RED);
+		answer4Button.setBackground(Color.YELLOW);
+		answer4Button.setBounds(357, 311, 159, 78);
+		frame.getContentPane().add(answer4Button);
 		
-		JRadioButton answer3RadioButton = new JRadioButton("New radio button");
-		answer3RadioButton.setBounds(227, 242, 18, 23);
-		contentPane.add(answer3RadioButton);
-		
-		JRadioButton answer2RadioButton = new JRadioButton("New radio button");
-		answer2RadioButton.setBounds(502, 179, 18, 23);
-		contentPane.add(answer2RadioButton);
-		
-		JRadioButton answer4RadioButton = new JRadioButton("New radio button");
-		answer4RadioButton.setBounds(502, 242, 18, 23);
-		contentPane.add(answer4RadioButton);
-		
-		JLabel userInstructionLabel = new JLabel("Please select the Correct Answer from the options below");
-		userInstructionLabel.setForeground(Color.RED);
-		userInstructionLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		userInstructionLabel.setBounds(45, 115, 361, 31);
-		contentPane.add(userInstructionLabel);
-		
-		JButton nextQuestionButton = new JButton("Next Question");
-		nextQuestionButton.setForeground(Color.RED);
-		nextQuestionButton.setBounds(431, 337, 118, 47);
-		contentPane.add(nextQuestionButton);
+		JLabel lblPleaseSelectAn = new JLabel("Please select an answer from the options below");
+		lblPleaseSelectAn.setBackground(Color.YELLOW);
+		lblPleaseSelectAn.setForeground(Color.RED);
+		lblPleaseSelectAn.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPleaseSelectAn.setBounds(50, 147, 429, 40);
+		frame.getContentPane().add(lblPleaseSelectAn);
 	}
 }
