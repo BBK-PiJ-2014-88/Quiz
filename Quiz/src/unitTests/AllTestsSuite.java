fcf8e9bc8dad169ddb0f99db17d17b0dcb146620
Sergio <rurounisergii@hotmail.com>
2015-02-23 17:38:52 +0000

    Added a TestSuite class that will contain all JUnit Tests

---
+++ Quiz/src/unitTests/AllTestsSuite.java
@@ -0,0 +1,11 @@
+package unitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ QuestionTest.class, QuizTest.class})
+public class AllTestsSuite {
+
+} 
